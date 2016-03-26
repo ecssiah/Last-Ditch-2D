7fa1f3b03e62d3af2632aea25717a3a06fc6f9f4
eks-siah <eks.siah@gmail.com>
2016-03-26 17:12:10 -0500

    updated ui constant names

--- src/systems/InventoryUISystem.cc
+++ src/systems/InventoryUISystem.cc
@@ -43,7 +43,7 @@ void InventoryUISystem::update()
   {
     if (element_hit_at(inventory_list, input.left_mouse_pressed_pos))
     {
-      inventory_list.scrolled_offset += INVENTORY_WHEEL_SCROLL_RATE * input.mouse_drag_vector.y();
+      inventory_list.scrolled_offset += INVENTORY_MOUSE_SCROLL_RATE * input.mouse_drag_vector.y();
       inventory_list.scrolled_offset =
 	std::max(-100, std::min(inventory_list.scrolled_offset, 0));
 
@@ -56,7 +56,7 @@ void InventoryUISystem::update()
 
     if (element)
     {
-      element->scrolled_offset += INVENTORY_SCROLL_RATE * input.mouse_drag_vector.y();
+      element->scrolled_offset += INVENTORY_MOUSE_SCROLL_RATE * input.mouse_drag_vector.y();
       element->scrolled_offset =
 	std::max(-100, std::min(element->scrolled_offset, 0));
 
@@ -68,7 +68,7 @@ void InventoryUISystem::update()
 
   if (input.mouse_wheel)
   {
-    inventory_list.scrolled_offset += INVENTORY_SCROLL_RATE * input.mouse_wheel_vector.y();
+    inventory_list.scrolled_offset += INVENTORY_MOUSE_SCROLL_RATE * input.mouse_wheel_vector.y();
     inventory_list.scrolled_offset =
       std::max(-100, std::min(inventory_list.scrolled_offset, 0));
 
@@ -100,17 +100,17 @@ void InventoryUISystem::setup()
 {
   menu_base.type = "backdrop1";
   menu_base.texture = "ui1";
-  menu_base.size = {SUB_MENU_BASE_SIZE_X, SUB_MENU_BASE_SIZE_Y};
+  menu_base.size = {MENU_BASE_SIZE_X, MENU_BASE_SIZE_Y};
   menu_base.pos =
-    {(SCREEN_SIZE_X - SUB_MENU_BASE_SIZE_X) / 2,
-     (SCREEN_SIZE_Y - SUB_MENU_BASE_SIZE_Y) / 2};
+    {(SCREEN_SIZE_X - MENU_BASE_SIZE_X) / 2,
+     (SCREEN_SIZE_Y - MENU_BASE_SIZE_Y) / 2};
 
   UIElement title;
   title.text = "Inventory";
   title.text_texture = "inventory-title-text";
   title.size = {200, 30};
   title.pos =
-    {menu_base.pos.x() + (SUB_MENU_BASE_SIZE_X - title.size.x()) / 2,
+    {menu_base.pos.x() + (MENU_BASE_SIZE_X - title.size.x()) / 2,
      menu_base.pos.y()};
 
   sdl_interface.create_texture_from_text(title.text, title.text_texture, "jura-medium");
--- src/systems/ProductionUISystem.cc
+++ src/systems/ProductionUISystem.cc
@@ -40,17 +40,17 @@ void ProductionUISystem::setup()
 {
   menu_base.type = "backdrop1";
   menu_base.texture = "ui1";
-  menu_base.size = {SUB_MENU_BASE_SIZE_X, SUB_MENU_BASE_SIZE_Y};
+  menu_base.size = {MENU_BASE_SIZE_X, MENU_BASE_SIZE_Y};
   menu_base.pos =
-    {(SCREEN_SIZE_X - SUB_MENU_BASE_SIZE_X) / 2,
-     (SCREEN_SIZE_Y - SUB_MENU_BASE_SIZE_Y) / 2};
+    {(SCREEN_SIZE_X - MENU_BASE_SIZE_X) / 2,
+     (SCREEN_SIZE_Y - MENU_BASE_SIZE_Y) / 2};
 
   UIElement title;
   title.text = "Production";
   title.text_texture = "production-title-text";
   title.size = {200, 30};
   title.pos =
-    {menu_base.pos.x() + (SUB_MENU_BASE_SIZE_X - title.size.x()) / 2,
+    {menu_base.pos.x() + (MENU_BASE_SIZE_X - title.size.x()) / 2,
      menu_base.pos.y()};
 
   sdl_interface.create_texture_from_text(title.text, title.text_texture, "jura-medium");
